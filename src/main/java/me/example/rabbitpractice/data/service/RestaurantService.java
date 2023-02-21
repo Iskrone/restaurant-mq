2f566c903bcf68663d6858289ae84327f6b86331
Alex <61450222+Iskrone@users.noreply.github.com>
2023-02-21 19:18:04 +0300

    smallfixes

--- src/main/java/me/example/rabbitpractice/data/service/RestaurantService.java
+++ src/main/java/me/example/rabbitpractice/data/service/RestaurantService.java
@@ -1,7 +1,6 @@
 package me.example.rabbitpractice.data.service;
 
 import me.example.rabbitpractice.data.model.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
